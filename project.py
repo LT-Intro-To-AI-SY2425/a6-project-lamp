464668b18dec8865e0ca30489eda93f3b6eb34ad
Nate Fulkerson <nfulkerson@cps.edu>
2025-01-07 11:26:54 -0600

    added stuff

--- project.py
+++ project.py
@@ -2,15 +2,15 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-data = pd.read_csv("a6-project-lamp/premier_league_data.csv")
+data = pd.read_csv("a6-project-lamp/premier-player-23-24.csv")
 x = data["Age"]
 y = data["GIs"]
 
 plt.figure(figsize=(5,4))
 
 plt.xlabel("Age")
-plt.ylabel("")
-plt.title(" by Age")
+plt.ylabel("Goals")
+plt.title("Goals by Age")
 plt.scatter(x, y)
 
 print("Pearson's Correlation: r = :", x.corr(y))
