488e3563e4903865afd21e8d2b5e00d26cbed997
lyogics <lyogi@cps.edu>
2025-01-09 11:09:25 -0600

    finished program

--- project.py
+++ project.py
@@ -1,18 +1,30 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
 
 data = pd.read_csv("premier-player-23-24.csv")
-x = data["Age"].values
+x = data[["Age", "Min", "90s"]].values
 y = data["Gls"].values
 
-plt.figure(figsize=(5,4))
+xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size = .2)
 
-plt.xlabel("Age")
-plt.ylabel("Goals")
-plt.title("Goals by Age")
-plt.scatter(x, y)
+model = LinearRegression().fit(xtrain, ytrain)
+coef = np.around(model.coef_, 2)
+intercept = round(float(model.intercept_))
+r_squared = round(model.score(x, y), 2)
 
-print("Pearson's Correlation: r = :", x.corr(y))
+print(f"Model's Linear Equation: y={coef[0]}x1 + {coef[1]}x2 + {coef[2]}x3 + {intercept}")
+print("R Squared Value: ", r_squared)
 
-plt.show()
+predict = model.predict(xtest)
+predict = np.around(predict, 2)
+print(predict)
+
+print("\nTesting Multivariable Model With Testing Data:")
+for index in range(len(xtest)):
+    actual = ytest[index]
+    predicted_y = predict[index]
+    x_coord = xtest[index]
+    print(f"Age: {x_coord[0]} Minutes Played: {x_coord[1]} Num of 90 Min Games: {x_coord[2]} Actual: {actual} Predicted {predicted_y}")
